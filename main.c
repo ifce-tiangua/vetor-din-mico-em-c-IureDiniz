f29fb6cf2108dfb2f92f155376fcb45bdbdeeb6c
IureDiniz <iuredinizsoares@gmail.com>
2024-11-07 15:44:50 -0300

    Resolvi o exercício sobre vetores

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-
-    //insira aqui seu código
+    int tam, *p;
+    scanf(" %d", &tam);
+    p = (int*) malloc(tam * sizeof(int));
+    for(int i = 0; i < tam; i++){
+        int aux;
+        scanf("%d", &aux);
+        p[i] = aux;
+    }
+    printf("[");
+    if(*p != NULL){
+        for(int i = 0; i < tam; i++){
+            if(i < tam-1)
+                printf("%d,", p[i]);
+            else
+                printf("%d", p[i]);
+        }
+    }else{
+        printf("vazio");
+    }
+    printf("]\n");
+    
+    free(p);
     printf("Nao implementado ainda...");
 
     return 0;
